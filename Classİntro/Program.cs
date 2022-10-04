bf2f933b3e13547373a60b6f28afee1547a433c2
ARD <ARD@DESKTOP-7HFLC8Q>
2022-10-04 15:19:06 +0300

    Proje dosyası ekle.

---
+++ Classİntro/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Classİntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Engin";
+            int yas = 36;
+
+            Kurs kurs1=new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmeni = "Engin";
+            kurs1.İzlenmeoranı = 68;
+
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmeni = "Kerem";
+            kurs2.İzlenmeoranı = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Pyhton";
+            kurs3.Egitmeni = "Berkay";
+            kurs3.İzlenmeoranı = 80;
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmeni);         
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+
+        public string Egitmeni { get; set; }
+
+        public int İzlenmeoranı { get; set; }
+
+
+
+    }
+
+
+
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    internal class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler.Sepete eklendi : " + urun.Adi);
+        }
+
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat,  int StokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urunAdi);
+        }
+
+    }
+}
